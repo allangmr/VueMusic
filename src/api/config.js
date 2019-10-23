3070584c1e463e0a3f2ba3266b46fd0e743b1c41
Allan Montilla <allangmr7@gmail.com>
2019-10-22 21:45:48 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,15 @@
+export default {
+
+    appName: 'spotify',
+    apikey: '9b49c0d18f6fb25b4417609a71c85aa0',
+    secret: '87a42c7511fa491e3a37e49108d2eb1e',
+    registeredTo: 'allangmr'
+
+}
+
+
+
+//Application name	spotify
+//API key	9b49c0d18f6fb25b4417609a71c85aa0
+//Shared secret	87a42c7511fa491e3a37e49108d2eb1e
+//Registered to	allangmr
---
+++ src/components/Artista.vue
@@ -0,0 +1,24 @@
+<template>
+  <li :class="artist">
+    <h2>
+      <a :href="artist.url" target="_blank">{{artist.name}}</a>
+    </h2>
+    <img :src="artist.image[2]['#text']" />
+  </li>
+</template>
+
+<script>
+export default {
+  name: "artista",
+  props: ["artist"]
+};
+</script>
+
+
+<style lang="stylus" scope>
+li.artist {
+  display: block;
+  margin: 0 10px;
+}
+</style>
+
